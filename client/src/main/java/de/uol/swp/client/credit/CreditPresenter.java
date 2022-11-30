f83314ff146a85152d44a68ce9cf9b4ba40d2bdb
TDang <tommy.dang@uni-oldenburg.de>
2022-11-30 16:45:38 +0100

    refactor: Revise CreditPResenter

--- client/src/main/java/de/uol/swp/client/credit/CreditPresenter.java
+++ client/src/main/java/de/uol/swp/client/credit/CreditPresenter.java
@@ -1,4 +1,35 @@
 package de.uol.swp.client.credit;
 
-public class CreditPresenter {
+import de.uol.swp.client.AbstractPresenter;
+import de.uol.swp.client.main.event.ShowMainMenuViewEvent;
+import de.uol.swp.common.user.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class CreditPresenter extends AbstractPresenter {
+    /**
+     * Manages the rulebook window
+     *
+     * @author Tommy Dang
+     * @see de.uol.swp.client.AbstractPresenter
+     * @since 2022-11-30
+     *
+     */
+
+    public static final String FXML = "/fxml/CreditView.fxml";
+
+    private User loggedInUser;
+
+    /**
+     * Default Constructor
+     *
+     * @since 2022-11-30
+     */
+    public CreditPresenter() {
+    }
+
+    @FXML
+    void onBackButtonPressed(ActionEvent event){
+        eventBus.post(new ShowMainMenuViewEvent());
+    }
 }
---
+++ client/src/main/java/de/uol/swp/client/option/OptionPresenter.java
@@ -0,0 +1,6 @@
+package de.uol.swp.client.option;
+
+import de.uol.swp.client.AbstractPresenter;
+
+public class OptionPresenter extends AbstractPresenter {
+}
