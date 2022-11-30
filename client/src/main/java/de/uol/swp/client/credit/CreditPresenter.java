4f54de6873ee7c3ba34fcf4bc02314e62663487d
TDang <tommy.dang@uni-oldenburg.de>
2022-11-30 20:39:41 +0100

    fix: Test failed

--- client/src/main/java/de/uol/swp/client/credit/CreditPresenter.java
+++ client/src/main/java/de/uol/swp/client/credit/CreditPresenter.java
@@ -15,6 +15,8 @@ public class CreditPresenter extends AbstractPresenter {
      *
      */
 
+    public static final String FXML = "/fxml/CreditView.fxml";
+
     /**
      * Default Constructor
      *
