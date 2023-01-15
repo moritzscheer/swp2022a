d239ed9fb2ddd25cde0c48a001e9c8e195fe6f0a
O1ez <ole.z@yahoo.com>
2023-01-15 17:40:31 +0100

    fix: correct credentials for the Database

--- server/src/main/java/de/uol/swp/server/database/SQLHelper.java
+++ server/src/main/java/de/uol/swp/server/database/SQLHelper.java
@@ -1,9 +1,9 @@
 package de.uol.swp.server.database;
 import java.sql.*;
 public class SQLHelper {
-    private static final String DB_URL = "jdbc:mysql://zimmermannnetwork.ddns.net:3306/owncloud";
-    private static final String DB_USER = "root";
-    private static final String DB_PASS = "swp2022A";
+    private static final String DB_URL = "jdbc:mysql://duemmer.informatik.uni-oldenburg.de:2101";
+    private static final String DB_USER = "swp2022A";
+    private static final String DB_PASS = "*softwareprojekt2022A*";
     private static Connection SqlConnection;
     public static Connection GetSqlConnection() throws SQLException {
 
