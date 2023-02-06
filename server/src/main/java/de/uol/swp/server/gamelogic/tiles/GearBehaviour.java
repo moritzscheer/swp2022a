a7964a64d59255bed793a9274f2fbfad2c56dc30
Maria Eduarda <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-02-06 14:11:46 +0100

    feat: implement turnRobot

--- server/src/main/java/de/uol/swp/server/gamelogic/tiles/GearBehaviour.java
+++ server/src/main/java/de/uol/swp/server/gamelogic/tiles/GearBehaviour.java
@@ -1,26 +1,79 @@
 package de.uol.swp.server.gamelogic.tiles;
 
+import de.uol.swp.server.gamelogic.Block;
+import de.uol.swp.server.gamelogic.Position;
+import de.uol.swp.server.gamelogic.Robot;
+import de.uol.swp.server.gamelogic.tiles.enums.CardinalDirection;
+
+import java.util.Objects;
 
 /**
+ * Manages the gear block operation over the robots
  *
- *
- * @author
- * @see
- * @since
+ * @author Maria Eduarda Costa Leite Andrade
+ * @see de.uol.swp.server.gamelogic.tiles.AbstractTileBehaviour
+ * @see de.uol.swp.server.gamelogic.tiles.enums.CardinalDirection
+ * @since 06-02-2023
  */
-public class GearBehaviour extends AbstractTileBehaviour{
+public class GearBehaviour extends AbstractTileBehaviour {
 
     private boolean turnClockwise;
 
     /**
+     * Constructor
      *
-     *
-     * @author
-     * @see
-     * @since
+     * @author Maria Eduarda Costa Leite Andrade
+     * @see de.uol.swp.server.gamelogic.tiles.AbstractTileBehaviour
+     * @since 06-02-2023
      */
-    public boolean turnRobot(){
-        //TODO
-        return turnClockwise;
+    public GearBehaviour(Robot[] robotStates, Block[][] board, Position blockPos, boolean turnC) {
+        super(robotStates, board, blockPos);
+        turnClockwise = turnC;
+    }
+
+    /**
+     * Turn the robot given the robot original direction
+     *
+     * @author Maria Eduarda Costa Leite Andrade
+     * @see de.uol.swp.server.gamelogic.tiles.enums.CardinalDirection
+     * @since 06-02-2023
+     */
+    public void turnRobot() {
+        for (Robot robotState : robotStates) {
+            if (Objects.equals(robotState.getPosition(), blockPos)) {
+                if (turnClockwise) {
+                    switch (robotState.getDirection()) {
+                        case North:
+                            robotState.setDirection(CardinalDirection.East);
+                            break;
+                        case East:
+                            robotState.setDirection(CardinalDirection.South);
+                            break;
+                        case South:
+                            robotState.setDirection(CardinalDirection.West);
+                            break;
+                        case West:
+                            robotState.setDirection(CardinalDirection.North);
+                            break;
+                    }
+                } else {
+                    switch (robotState.getDirection()) {
+                        case North:
+                            robotState.setDirection(CardinalDirection.West);
+                            break;
+                        case East:
+                            robotState.setDirection(CardinalDirection.North);
+                            break;
+                        case South:
+                            robotState.setDirection(CardinalDirection.East);
+                            break;
+                        case West:
+                            robotState.setDirection(CardinalDirection.South);
+                            break;
+                    }
+                }
+                return;
+            }
+        }
     }
 }
