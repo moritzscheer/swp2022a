39cc957b9a1e854b1278bdf817e0fcc523d792b8
Ole Zimmermann <ole.zimmermann@uni-oldenburg.de>
2023-03-13 15:54:54 +0100

    Implement WallBehaviour and Block class

--- server/src/main/java/de/uol/swp/server/gamelogic/tiles/WallBehaviour.java
+++ server/src/main/java/de/uol/swp/server/gamelogic/tiles/WallBehaviour.java
@@ -5,27 +5,30 @@ import de.uol.swp.server.gamelogic.Position;
 import de.uol.swp.server.gamelogic.Robot;
 import de.uol.swp.server.gamelogic.tiles.enums.CardinalDirection;
 
-/**
- * @author
- * @see
- * @since
- */
+/** @author Ole Zimemrmann */
 public class WallBehaviour extends AbstractTileBehaviour {
 
     private CardinalDirection wallDirection;
 
-    public WallBehaviour(Robot[] robotStates, Block[][] board, Position blockPos) {
+    public WallBehaviour(
+            Robot[] robotStates,
+            Block[][] board,
+            Position blockPos,
+            CardinalDirection wallDirection) {
         super(robotStates, board, blockPos);
-        // TODO
+        this.wallDirection = wallDirection;
     }
 
     /**
-     * @author
-     * @see
-     * @since
+     * @author Ole Zimmermann
+     * @see de.uol.swp.server.gamelogic.tiles.AbstractTileBehaviour
+     * @since 13-03-2023
      */
-    public CardinalDirection getWall() {
-        // TODO
-        return wallDirection;
+    @Override
+    public boolean getObstruction(CardinalDirection direction) {
+        if (direction == this.wallDirection) {
+            return true;
+        }
+        return false;
     }
 }
