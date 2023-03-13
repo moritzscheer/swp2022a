dd6fc87a77fc1084837ad0c4aae9c3809a9f3758
Maria Eduarda <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-03-13 17:46:06 +0100

    test: add ConveyorBelt test

--- server/src/main/java/de/uol/swp/server/gamelogic/tiles/ExpressConveyorBeltBehaviour.java
+++ server/src/main/java/de/uol/swp/server/gamelogic/tiles/ExpressConveyorBeltBehaviour.java
@@ -4,13 +4,20 @@ import de.uol.swp.server.gamelogic.Block;
 import de.uol.swp.server.gamelogic.MoveIntent;
 import de.uol.swp.server.gamelogic.Position;
 import de.uol.swp.server.gamelogic.Robot;
+import de.uol.swp.server.gamelogic.tiles.enums.ArrowType;
+import de.uol.swp.server.gamelogic.tiles.enums.CardinalDirection;
 
 import java.util.List;
 
 public class ExpressConveyorBeltBehaviour extends ConveyorBeltBehaviour {
 
-    public ExpressConveyorBeltBehaviour(Robot[] robotStates, Block[][] board, Position blockPos) {
-        super(robotStates, board, blockPos);
+    public ExpressConveyorBeltBehaviour(
+            Robot[] robotStates,
+            Block[][] board,
+            Position blockPos,
+            ArrowType arrowType,
+            CardinalDirection direction) {
+        super(robotStates, board, blockPos, arrowType, direction);
     }
 
     @Override
