3c64579dfa01122d8c5ba0c4d6f01e6ca2e8e1ad
Maria Eduarda <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-04-22 17:43:43 +0200

    feat: add moves variable to MoveIntent

--- server/src/main/java/de/uol/swp/server/gamelogic/MoveIntent.java
+++ server/src/main/java/de/uol/swp/server/gamelogic/MoveIntent.java
@@ -5,13 +5,25 @@ import de.uol.swp.server.gamelogic.tiles.enums.CardinalDirection;
 public class MoveIntent {
     public final int robotID;
     public final CardinalDirection direction;
+    public final int moves;
 
     public MoveIntent(int id, CardinalDirection dir) {
-        robotID = id;
-        direction = dir;
+        this.robotID = id;
+        this.direction = dir;
+        this.moves = 1;
+    }
+
+    public MoveIntent(int id, CardinalDirection dir, int moves) {
+        this.robotID = id;
+        this.direction = dir;
+        this.moves = moves;
     }
 
     public CardinalDirection getDirection() {
-        return direction;
+        return this.direction;
+    }
+
+    public int getMoves() {
+        return this.moves;
     }
 }
