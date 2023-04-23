0ec8cfd5d8d1795b6910ca24071993fa8a93367a
Maria Eduarda <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-04-23 17:12:05 +0200

    refactor: remove moves, create one move intent each time

--- server/src/main/java/de/uol/swp/server/gamelogic/MoveIntent.java
+++ server/src/main/java/de/uol/swp/server/gamelogic/MoveIntent.java
@@ -5,33 +5,13 @@ import de.uol.swp.server.gamelogic.tiles.enums.CardinalDirection;
 public class MoveIntent {
     public final int robotID;
     public final CardinalDirection direction;
-    public final int moves;
 
     public MoveIntent(int id, CardinalDirection dir) {
         this.robotID = id;
         this.direction = dir;
-        this.moves = 1;
-    }
-
-    /**
-     * Constructor with moves
-     *
-     * <p>This is necessary to handle movements with cards, which may move 1, 2 or 3 positions
-     *
-     * @author Maria Eduarda
-     * @since 2023-04-22
-     */
-    public MoveIntent(int id, CardinalDirection dir, int moves) {
-        this.robotID = id;
-        this.direction = dir;
-        this.moves = moves;
     }
 
     public CardinalDirection getDirection() {
         return this.direction;
-    }
-
-    public int getMoves() {
-        return this.moves;
     }
 }
