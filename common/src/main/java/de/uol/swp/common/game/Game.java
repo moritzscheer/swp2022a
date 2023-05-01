29700fd0d2a61efb403bc9f8c1ad3cb7128dfe91
Maria Eduarda <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-01 16:59:50 +0200

    refactor: add game folder in common

---
+++ common/src/main/java/de/uol/swp/common/game/Game.java
@@ -0,0 +1,23 @@
+package de.uol.swp.common.game;
+
+import java.io.Serializable;
+
+/**
+ * Interface to unify game objects
+ *
+ * <p>This is an Interface to allow for multiple types of game objects since it is possible that not
+ * every client has to have every information of the game.
+ *
+ * @author Maria Eduarda Costa Leite Andrade
+ * @since 2023-05-01
+ */
+public interface Game extends Serializable {
+
+    /**
+     * Getter for gameID of the game
+     *
+     * @return an Integer containing the game ID
+     * @since 2023-05-01
+     */
+    Integer getGameID();
+}
