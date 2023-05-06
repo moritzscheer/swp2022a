d752e2712fbe23be80a4b4d0e422b6185d762848
Maria Andrade <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-06 22:15:09 +0200

    feat: implement gamedto

--- common/src/main/java/de/uol/swp/common/game/dto/GameDTO.java
+++ common/src/main/java/de/uol/swp/common/game/dto/GameDTO.java
@@ -1,4 +1,25 @@
 package de.uol.swp.common.game.dto;
 
+import de.uol.swp.common.game.Game;
+
 // TODO
-public class GameDTO {}
+public class GameDTO implements Game {
+    private final Integer gameID;
+
+    /**
+     * Constructor
+     *
+     * @param gameID The id of the card
+     * @author Maria Eduarda Costa Leite Andrade
+     * @since 2023-05-02
+     */
+    public GameDTO(Integer gameID) {
+        this.gameID = gameID;
+    }
+
+    @Override
+    public Integer getGameID() {
+        return this.gameID;
+    }
+
+}
