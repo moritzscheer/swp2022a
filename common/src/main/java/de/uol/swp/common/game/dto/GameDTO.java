a6df6c1b4eb3a5bddac3827fe1951929b6db389c
Maria Andrade <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-13 18:04:43 +0200

    feat: add players and board in gameDTO

--- common/src/main/java/de/uol/swp/common/game/dto/GameDTO.java
+++ common/src/main/java/de/uol/swp/common/game/dto/GameDTO.java
@@ -2,19 +2,29 @@ package de.uol.swp.common.game.dto;
 
 import de.uol.swp.common.game.Game;
 
+import java.util.List;
+
 // TODO
 public class GameDTO implements Game {
     private final Integer gameID;
 
+    private List<PlayerDTO> players;
+
+    private BoardDTO boardDTO;
+
     /**
      * Constructor
      *
-     * @param gameID The id of the card
+     * @param gameID   The id of the card
+     * @param players  Players in the lobby
+     * @param boardDTO Board of the game
      * @author Maria Eduarda Costa Leite Andrade
      * @since 2023-05-02
      */
-    public GameDTO(Integer gameID) {
+    public GameDTO(Integer gameID, List<PlayerDTO> players, BoardDTO boardDTO) {
         this.gameID = gameID;
+        this.players = players;
+        this.boardDTO = boardDTO;
     }
 
     @Override
@@ -22,4 +32,47 @@ public class GameDTO implements Game {
         return this.gameID;
     }
 
+    /**
+     * Getter for boardDTO
+     *
+     * @author Maria Eduarda Costa Leite Andrade
+     * @see de.uol.swp.common.game.dto.BoardDTO
+     * @since 2023-05-13
+     */
+    public BoardDTO getBoardDTO() {
+        return boardDTO;
+    }
+
+    /**
+     * Setter for boardDTO
+     *
+     * @author Maria Eduarda Costa Leite Andrade
+     * @see de.uol.swp.common.game.dto.BoardDTO
+     * @since 2023-05-13
+     */
+    public void setBoardDTO(BoardDTO boardDTO) {
+        this.boardDTO = boardDTO;
+    }
+
+    /**
+     * Getter for list of Players
+     *
+     * @author Maria Eduarda Costa Leite Andrade
+     * @see de.uol.swp.common.game.dto.PlayerDTO
+     * @since 2023-05-13
+     */
+    public List<PlayerDTO> getPlayers() {
+        return players;
+    }
+
+    /**
+     * Setter for list of Players
+     *
+     * @author Maria Eduarda Costa Leite Andrade
+     * @see de.uol.swp.common.game.dto.PlayerDTO
+     * @since 2023-05-13
+     */
+    public void setPlayers(List<PlayerDTO> players) {
+        this.players = players;
+    }
 }
