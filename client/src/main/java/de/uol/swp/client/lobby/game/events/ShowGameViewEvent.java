84b5aaa88a9018049fb138e64c4b1d8a75118245
Maria Andrade <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-14 19:58:59 +0200

    refactor: simplify event

--- client/src/main/java/de/uol/swp/client/lobby/game/events/ShowGameViewEvent.java
+++ client/src/main/java/de/uol/swp/client/lobby/game/events/ShowGameViewEvent.java
@@ -1,6 +1,4 @@
 package de.uol.swp.client.lobby.game.events;
-
-import de.uol.swp.common.game.dto.GameDTO;
 import de.uol.swp.common.lobby.dto.LobbyDTO;
 
 /**
@@ -17,15 +15,10 @@ public class ShowGameViewEvent {
 
     private LobbyDTO lobby;
     private int gameID;
-    private final int[][][][] boardImageIds;
 
-    private GameDTO game;
-
-    public ShowGameViewEvent(LobbyDTO lobby, int gameID, int[][][][] boardImageIds, GameDTO game) {
+    public ShowGameViewEvent(LobbyDTO lobby, int gameID) {
         this.lobby = lobby;
         this.gameID = gameID;
-        this.boardImageIds = boardImageIds;
-        this.game = game;
     }
 
     public Integer getLobbyID() {
@@ -40,11 +33,4 @@ public class ShowGameViewEvent {
         return gameID;
     }
 
-    public int[][][][] getBoardImageIds() {
-        return boardImageIds;
-    }
-
-    public GameDTO getGame() {
-        return game;
-    }
 }
