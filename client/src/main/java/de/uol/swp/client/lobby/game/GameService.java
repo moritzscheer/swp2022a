8072281b68fa22e5d599f8b83b1712add2346b74
Maria Andrade <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-15 02:56:45 +0200

    refactor: make GameService handle all requests/response

--- client/src/main/java/de/uol/swp/client/lobby/game/GameService.java
+++ client/src/main/java/de/uol/swp/client/lobby/game/GameService.java
@@ -1,11 +1,21 @@
 package de.uol.swp.client.lobby.game;
 
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import de.uol.swp.client.lobby.LobbyGameManagement;
+import de.uol.swp.client.lobby.LobbyManagement;
+import de.uol.swp.client.lobby.game.events.RequestMapDataEvent;
+import de.uol.swp.client.lobby.game.presenter.GamePresenter;
+import de.uol.swp.common.game.message.GetMapDataResponse;
+import de.uol.swp.common.game.message.StartGameMessage;
 import de.uol.swp.common.game.request.GetMapDataRequest;
 import de.uol.swp.common.game.request.GetProgramCardsRequest;
 import de.uol.swp.common.game.request.StartGameRequest;
 import de.uol.swp.common.lobby.dto.LobbyDTO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Classes that manages game
@@ -14,13 +24,15 @@ import de.uol.swp.common.lobby.dto.LobbyDTO;
  * @since 2023-05-06
  */
 @SuppressWarnings("UnstableApiUsage")
+@Singleton
 public class GameService {
     private final EventBus eventBus;
+    private static final Logger LOG = LogManager.getLogger(GameService.class);
 
     /**
      * Constructor
      *
-     * @param eventBus The EventBus set in ClientModule
+     * @param eventBus        The EventBus set in ClientModule
      * @see de.uol.swp.client.di.ClientModule
      * @since 2023-05-06
      */
@@ -29,6 +41,10 @@ public class GameService {
         this.eventBus = eventBus;
         this.eventBus.register(this);
     }
+
+    ///////////////
+    // Requests
+    ///////////////
 
     /**
      * Posts a request to start the game on the EventBus
@@ -39,14 +55,15 @@ public class GameService {
      * @since 2023-03-09
      */
     public void startGame(LobbyDTO lobby) {
-        System.out.println("Starting Game");
+        LOG.debug("Starting Game");
         StartGameRequest startGameRequest = new StartGameRequest(lobby);
         eventBus.post(startGameRequest);
     }
 
-    public void getMapData(Integer gameID, LobbyDTO lobby) {
-        eventBus.post(new GetMapDataRequest(lobby, gameID));
-        System.out.println("Getting Map");
+    @Subscribe
+    public void onRequestMapDataEvent(RequestMapDataEvent event) {
+        LOG.debug("Getting Map");
+        eventBus.post(new GetMapDataRequest(event.getLobbyDTO()));
     }
 
     /** Get cards 9-5 Cards for each player
@@ -59,9 +76,43 @@ public class GameService {
      * @since 2023-05-06
      */
     public void getProgramCardsForPlayers(Integer gameID) {
+        LOG.debug("Getting Cards");
         GetProgramCardsRequest getProgramCardsRequest =
                 new GetProgramCardsRequest(gameID);
         eventBus.post(getProgramCardsRequest);
-        System.out.println("Getting Cards");
+    }
+
+    /////////////////////
+    // Responses/Messages
+    /////////////////////
+
+    /**
+     * Handles StartGameMessage detected on the EventBus
+     *
+     * <p>If a StartGameMessage is detected on the EventBus, this method gets called.
+     *
+     * @param msg The StartGameMessage detected on the EventBus
+     * @see de.uol.swp.common.game.message.StartGameMessage
+     * @author Moritz Scheer & Maxim Erden & Maria Eduarda
+     * @since 2023-02-28
+     */
+    @Subscribe
+    public void onStartGameMessage(StartGameMessage msg){
+        LOG.debug("onStartGameMessage");
+        LobbyManagement.getInstance().startGame(msg);
+    }
+
+    /**
+     * Handles GetMapDataMessage
+     *
+     * @param msg the GetMapDataMessage object seen on the EventBus
+     * @see GetMapDataResponse
+     * @author Maria Andrade
+     * @since 2023-05-06
+     */
+    @Subscribe
+    public void onGetMapDataResponse(GetMapDataResponse msg){
+        LOG.debug("onGetMapDataResponse");
+        LobbyManagement.getInstance().reloadMapData(msg);
     }
 }
