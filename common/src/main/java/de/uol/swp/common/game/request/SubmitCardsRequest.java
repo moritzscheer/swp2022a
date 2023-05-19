51cba0c84881348dec0743898e501037a58f8371
Maria Andrade <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-19 16:39:12 +0200

    feat: implement submit cards request

--- common/src/main/java/de/uol/swp/common/game/request/SubmitCardsRequest.java
+++ common/src/main/java/de/uol/swp/common/game/request/SubmitCardsRequest.java
@@ -1,29 +1,53 @@
 package de.uol.swp.common.game.request;
 
+import de.uol.swp.common.game.dto.CardDTO;
 import de.uol.swp.common.message.AbstractRequestMessage;
+import de.uol.swp.common.user.UserDTO;
+
+import java.util.List;
 
 public class SubmitCardsRequest extends AbstractRequestMessage {
     private static final long serialVersionUID = -5910486927806761643L;
-    private final int[] cardIDs;
+    private final List<CardDTO> cardDTO;
+    private final int lobbyID;
+    private final UserDTO userDTO;
 
     /**
      * @author Ole Zimmermann
      * @since 2023-04-25
      */
-    public SubmitCardsRequest(int[] cardIDs) {
-        this.cardIDs = cardIDs;
+    public SubmitCardsRequest(int lobbyID, UserDTO userDTO, List<CardDTO>  cardDTOS) {
+        this.lobbyID = lobbyID;
+        this.userDTO = userDTO;
+        this.cardDTO = cardDTOS;
     }
 
     /**
      * @author Ole Zimmermann
      * @since 2023-04-25
      */
-    public int[] getCardIDs() {
-        return cardIDs;
+    public List<CardDTO>  getCardDTOs() {
+        return cardDTO;
     }
 
     @Override
     public boolean authorizationNeeded() {
         return true;
+    }
+
+    /**
+     * @author Maria Andrade
+     * @since 2023-05-18
+     */
+    public int getLobbyID() {
+        return lobbyID;
+    }
+
+    /**
+     * @author Maria Andrade
+     * @since 2023-05-18
+     */
+    public UserDTO getUserDTO() {
+        return userDTO;
     }
 }
