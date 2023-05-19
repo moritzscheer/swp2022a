cf198f919747782232fa3d9e893704f4498caa81
Maria Andrade <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-19 11:07:35 +0200

    feat: use id instead of imgPath

--- server/src/main/java/de/uol/swp/server/gamelogic/Robot.java
+++ server/src/main/java/de/uol/swp/server/gamelogic/Robot.java
@@ -35,8 +35,8 @@ public class Robot implements Serializable {
      * @since 06-02-2023
      */
     public Robot(
-            String imgPath, Position currentPosition, boolean alive, CardinalDirection direction) {
-        this.imgPath = imgPath;
+            int id, Position currentPosition, boolean alive, CardinalDirection direction) {
+        this.id = id;
         this.currentPosition = currentPosition;
         this.alive = alive;
         this.damageToken = 0;
