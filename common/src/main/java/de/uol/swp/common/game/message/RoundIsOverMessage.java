20ac31e7a2afb1a589145149694f98e3c8b2005f
Maria Andrade <maria.eduarda.costa.leite.andrade@uni-oldenburg.de>
2023-05-24 12:01:54 +0200

    docs: add docs

--- common/src/main/java/de/uol/swp/common/game/message/RoundIsOverMessage.java
+++ common/src/main/java/de/uol/swp/common/game/message/RoundIsOverMessage.java
@@ -2,6 +2,12 @@ package de.uol.swp.common.game.message;
 
 import de.uol.swp.common.lobby.message.AbstractLobbyMessage;
 
+/**
+ * Message to distribute new cards and reset view for players
+ *
+ * @author Maria Eduarda Costa Leite Andrade
+ * @since 2023-05-24
+ */
 public class RoundIsOverMessage extends AbstractLobbyMessage {
     private final int lobbyID;
 
