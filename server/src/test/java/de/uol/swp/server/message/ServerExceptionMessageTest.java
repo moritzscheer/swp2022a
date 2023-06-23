b2ed8cb7476c90ab3a9a68977e40374f01d01c24
WKempel <waldemar.kempel@uni-oldenburg.de>
2023-06-23 17:44:02 +0200

    Add: add some tests in : ClientAuthorizedMessageTest, LobbyServiceTest, ServerExceptionMessageTest

---
+++ server/src/test/java/de/uol/swp/server/message/ServerExceptionMessageTest.java
@@ -0,0 +1,42 @@
+package de.uol.swp.server.message;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class ServerExceptionMessageTest {
+
+    @Test
+    public void testGetException() {
+        Exception exception = new RuntimeException("Test exception");
+        ServerExceptionMessage message = new ServerExceptionMessage(exception);
+
+        Exception actualException = message.getException();
+
+        Assertions.assertEquals(exception, actualException);
+    }
+
+    @Test
+    public void testEquals() {
+        Exception exception1 = new RuntimeException("Exception 1");
+        Exception exception2 = new RuntimeException("Exception 2");
+
+        ServerExceptionMessage message1 = new ServerExceptionMessage(exception1);
+        ServerExceptionMessage message2 = new ServerExceptionMessage(exception1);
+        ServerExceptionMessage message3 = new ServerExceptionMessage(exception2);
+
+        // Same instance
+        Assertions.assertEquals(message1, message1);
+
+        // Same attributes but different instance
+        Assertions.assertEquals(message1, message2);
+
+        // Same attributes but different sequence
+        Assertions.assertEquals(message2, message1);
+
+        Assertions.assertEquals(message1.hashCode(), message2.hashCode());
+
+        Assertions.assertNotEquals(message1, message3);
+        Assertions.assertNotEquals(message3, message1);
+    }
+}
