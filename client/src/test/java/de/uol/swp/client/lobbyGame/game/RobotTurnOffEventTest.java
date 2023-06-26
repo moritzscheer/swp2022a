08bd60ff862902e04957aaa4bdd47ec75dfd67bd
WKempel <waldemar.kempel@uni-oldenburg.de>
2023-06-26 13:05:09 +0200

    Add: some tests: RequestDistributeCardsEventTest, RequestMapDataEventTest, RequestStartGameEventTest, RobotTurnOffEventTest, ShowGameOverEventTest, ShowGameViewEventTest

---
+++ client/src/test/java/de/uol/swp/client/lobbyGame/game/RobotTurnOffEventTest.java
@@ -0,0 +1,25 @@
+package de.uol.swp.client.lobbyGame.game;
+
+import de.uol.swp.client.lobbyGame.game.events.RobotTurnOffEvent;
+import de.uol.swp.common.lobby.dto.LobbyDTO;
+import de.uol.swp.common.user.UserDTO;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RobotTurnOffEventTest {
+
+    private final UserDTO loggedInUser = new UserDTO("player1","pw1","ml1");
+    private final UUID uuid = UUID.randomUUID();
+    private final LobbyDTO lobbyDTO = new LobbyDTO(123,"testLobby",loggedInUser,"pw",true,uuid);
+
+    @Test
+    public void testConstructorAndGetters() {
+        RobotTurnOffEvent event = new RobotTurnOffEvent(lobbyDTO.getLobbyID(), loggedInUser);
+
+        assertEquals(lobbyDTO.getLobbyID(), event.getLobbyID());
+        assertEquals(loggedInUser, event.getLoggedInUser());
+    }
+}
