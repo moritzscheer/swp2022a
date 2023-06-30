3b2a1393102cca6e990e181e1f36076ca164217e
Merden <maxim.erden@uni-oldenburg.de>
2023-06-30 09:57:27 +0200

    MapThree done, Checkpoints TODO

--- server/src/main/java/de/uol/swp/server/gamelogic/map/MapThree.java
+++ server/src/main/java/de/uol/swp/server/gamelogic/map/MapThree.java
@@ -69,7 +69,9 @@ public class MapThree extends AbstractMap{
         y = 2;
 
         x = 0;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y,new PusherBehaviour(null, map, new Position(x, y),new int[]{2,4}, CardinalDirection.West),
+                                new WallBehaviour(null, map, new Position(x,y), CardinalDirection.West));
+
         x = 1;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 2;
@@ -91,7 +93,8 @@ public class MapThree extends AbstractMap{
         x = 10;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 11;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y,new PusherBehaviour(null, map, new Position(x, y),new int[]{2,4}, CardinalDirection.East),
+                                new WallBehaviour(null, map, new Position(x,y), CardinalDirection.East));
 
         y = 3;
 
@@ -106,7 +109,8 @@ public class MapThree extends AbstractMap{
         x = 4;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.East));
         x = 5;
-        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.East));
+        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.East),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.North));
         x = 6;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.East));
         x = 7;
@@ -119,9 +123,11 @@ public class MapThree extends AbstractMap{
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 11;
         generateBlock(map, x, y);
+
         y = 4;
         x = 0;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y,new PusherBehaviour(null, map, new Position(x, y),new int[]{1,3,5}, CardinalDirection.West),
+                                new WallBehaviour(null, map, new Position(x,y), CardinalDirection.West));
         x = 1;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 2;
@@ -133,7 +139,8 @@ public class MapThree extends AbstractMap{
         x = 5;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.East));
         x = 6;
-        generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
+        generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.North));
         x = 7;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 8;
@@ -143,7 +150,8 @@ public class MapThree extends AbstractMap{
         x = 10;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 11;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y,new PusherBehaviour(null, map, new Position(x, y),new int[]{1,3,5}, CardinalDirection.East),
+                                new WallBehaviour(null, map, new Position(x,y), CardinalDirection.East));
         y = 5;
 
         x = 0;
@@ -157,9 +165,9 @@ public class MapThree extends AbstractMap{
         x = 4;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 5;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PitBehaviour(null, map, new Position(x,y)));
         x = 6;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PitBehaviour(null, map, new Position(x,y)));
         x = 7;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 8;
@@ -180,17 +188,21 @@ public class MapThree extends AbstractMap{
         x = 2;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 3;
-        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
+        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.West));
         x = 4;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 5;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PitBehaviour(null, map, new Position(x,y)));
         x = 6;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PitBehaviour(null, map, new Position(x,y)));
         x = 7;
-        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
+        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.East),
+                                 new LaserBehaviour(null, map, new int[]{1, 2, 3, 4, 5}, new Position(x, y), CardinalDirection.West, 1, true, true));
         x = 8;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
+
         x = 9;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 10;
@@ -201,7 +213,8 @@ public class MapThree extends AbstractMap{
         y = 7;
 
         x = 0;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y,new PusherBehaviour(null, map, new Position(x, y),new int[]{1,3,5}, CardinalDirection.West),
+                                new WallBehaviour(null, map, new Position(x,y), CardinalDirection.West));
         x = 1;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 2;
@@ -211,7 +224,9 @@ public class MapThree extends AbstractMap{
         x = 4;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 5;
-        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.TurnRight, CardinalDirection.North));
+        generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.TurnRight, CardinalDirection.North),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.South),
+                                 new LaserBehaviour(null, map, new int[]{1, 2, 3, 4, 5}, new Position(x, y), CardinalDirection.North, 1, true, true));
         x = 6;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.West));
         x = 7;
@@ -223,7 +238,8 @@ public class MapThree extends AbstractMap{
         x = 10;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 11;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PusherBehaviour(null, map, new Position(x, y),new int[]{1,3,5}, CardinalDirection.East),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.East));
 
         y = 8;
 
@@ -240,7 +256,9 @@ public class MapThree extends AbstractMap{
         x = 5;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.West));
         x = 6;
-        generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.West));
+        generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.West),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.South),
+                                 new LaserBehaviour(null, map, new int[]{1, 2, 3, 4, 5}, new Position(x, y), CardinalDirection.North, 1, true, true));
         x = 7;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.West));
         x = 8;
@@ -255,7 +273,8 @@ public class MapThree extends AbstractMap{
         y = 9;
 
         x = 0;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PusherBehaviour(null, map, new Position(x, y),new int[]{2,4}, CardinalDirection.West),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.West));
         x = 1;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 2;
@@ -277,7 +296,8 @@ public class MapThree extends AbstractMap{
         x = 10;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 11;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PusherBehaviour(null, map, new Position(x, y),new int[]{2,4}, CardinalDirection.East),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.East));
 
         y = 10;
 
@@ -309,25 +329,29 @@ public class MapThree extends AbstractMap{
         y = 11;
 
         x = 0;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new RepairBehaviour(null, map, new Position(x,y), 1));
         x = 1;
         generateBlock(map, x, y);
         x = 2;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PusherBehaviour(null, map, new Position(x, y),new int[]{2,4}, CardinalDirection.South),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.South));
         x = 3;
         generateBlock(map, x, y);
         x = 4;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PusherBehaviour(null, map, new Position(x, y),new int[]{1,3,5}, CardinalDirection.South),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.South));
         x = 5;
         generateBlock(map, x, y, new ConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.South));
         x = 6;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 7;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PusherBehaviour(null, map, new Position(x, y),new int[]{1,3,5}, CardinalDirection.South),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.South));
         x = 8;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new RepairBehaviour(null, map, new Position(x,y), 2));
         x = 9;
-        generateBlock(map, x, y);
+        generateBlock(map, x, y, new PusherBehaviour(null, map, new Position(x, y),new int[]{2,4}, CardinalDirection.South),
+                                 new WallBehaviour(null, map, new Position(x,y), CardinalDirection.South));
         x = 10;
         generateBlock(map, x, y, new ExpressConveyorBeltBehaviour(null, map, new Position(x, y), ArrowType.Straight, CardinalDirection.North));
         x = 11;
